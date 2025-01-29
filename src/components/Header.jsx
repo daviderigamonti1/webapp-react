cc61360c1ca87e7c79eeb20e9bbd6fd32d38c8ea
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-01-29 19:51:05 +0100

    implemented navbar toggle for mobile responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function Header() {
+    const [show, setShow] = useState(false);
+    function toggleNav() {
+        show ? setShow(false) : setShow(true);
+    }
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <a className="navbar-brand" href="#"><img src="" alt="" /></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"
+                        onClick={toggleNav}>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className={`collapse navbar-collapse ${show ? "show" : ""}`} id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <NavLink className="nav-link" aria-current="page" to="/">
+                                <NavLink className="nav-link active" aria-current="page" to="/">
                                     Home
                                 </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" aria-current="page" to="/movies">
-                                    Lista Movies
+                                <NavLink className="nav-link" to="/movies">
+                                    Lista movies
                                 </NavLink>
                             </li>
                             <li className="nav-item">
